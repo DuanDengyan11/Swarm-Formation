9bd944ce007d24d71641821339ec23cbaa2c7997
DuanDengyan11 <2560104196@qq.com>
2022-11-08 20:04:33 +0800

    20221108

--- src/planner/cable_load/src/cable_load.cpp
+++ src/planner/cable_load/src/cable_load.cpp
@@ -15,17 +15,17 @@ void cable_load::init(ros::NodeHandle &nh)
     cable_points.push_back({half_length, half_width, -half_height});
     cable_points.push_back({half_length, -half_width, -half_height});
 
-    Eigen::Matrix<double, 6, 12> G;
-    for (size_t i = 0; i < cable_points.size(); i++)
-    {
-        G.block<3,3>(0,3*i) << 1.0, 0.0, 0.0,
-        0.0, 1.0, 0.0,
-        0.0, 0.0, 1.0;
-        G.block<3,3>(3,3*i) = cal_skew_matrix(cable_points[i]);
-    }
-    G_inv = G.transpose() * (G * G.transpose()).inverse();  //不变的量
-    Eigen::FullPivLU<Eigen::MatrixXd> lu(G);
-    G_null_space = lu.kernel(); //不变量 G的一组基
+    // Eigen::Matrix<double, 6, 12> G;
+    // for (size_t i = 0; i < cable_points.size(); i++)
+    // {
+    //     G.block<3,3>(0,3*i) << 1.0, 0.0, 0.0,
+    //     0.0, 1.0, 0.0,
+    //     0.0, 0.0, 1.0;
+    //     G.block<3,3>(3,3*i) = cal_skew_matrix(cable_points[i]);
+    // }
+    // G_inv = G.transpose() * (G * G.transpose()).inverse();  //不变的量
+    // Eigen::FullPivLU<Eigen::MatrixXd> lu(G);
+    // G_null_space = lu.kernel(); //不变量 G的一组基
     
 }
 
