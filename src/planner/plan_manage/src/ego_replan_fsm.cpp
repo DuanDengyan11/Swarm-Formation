8e058c9de272dbc4ea59c1a0fea79b7187b89928
DuanDengyan11 <2560104196@qq.com>
2022-11-08 21:03:26 +0800

    20221108

--- src/planner/plan_manage/src/ego_replan_fsm.cpp
+++ src/planner/plan_manage/src/ego_replan_fsm.cpp
@@ -350,18 +350,18 @@ namespace ego_planner
     LocalTrajData *info = &planner_manager_->traj_.local_traj;
 
     Eigen::MatrixXd accs = info->traj.getAccs();
-    // Eigen::MatrixXd positions = info->traj.getPositions();
-    // Eigen::VectorXd durations = info->traj.getDurations();
+    Eigen::MatrixXd positions = info->traj.getPositions();
+    Eigen::VectorXd durations = info->traj.getDurations();
     
-    //先装作无人机姿态角不变， 其实可以通过微分平坦算出来
+    // 先装作无人机姿态角不变， 其实可以通过微分平坦算出来
 
-    // bool plan_success = planner_manager_->ReboundReplanForCable(accs, positions, durations);
+    bool plan_success = planner_manager_->ReboundReplanForCable(accs, positions, durations);
 
-    // if (plan_success)
-    // {
-    //   //不确定是否需要将规划成功的轨迹 发布出去
-    // }
-    // return plan_success;
+    if (plan_success)
+    {
+      //不确定是否需要将规划成功的轨迹 发布出去
+    }
+    return plan_success;
     return true;
   }
 
