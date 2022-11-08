91de650a4cfbaaf87642fb6c31781e5a483599ea
DuanDengyan11 <2560104196@qq.com>
2022-11-08 19:38:43 +0800

    20221108

--- src/planner/plan_manage/src/ego_replan_fsm.cpp
+++ src/planner/plan_manage/src/ego_replan_fsm.cpp
@@ -18,35 +18,35 @@ namespace ego_planner
     have_local_traj_cable_ = false; //有没有上次规划好的local cable轨迹
     flag_relan_astar_ = true; //是否需要重新astar规划
 
-    /*  fsm param  */
-    nh.param("fsm/thresh_replan_time", replan_thresh_, -1.0);  // = 1s
-    nh.param("fsm/thresh_no_replan_meter", no_replan_thresh_, -1.0); // = 1m
-    nh.param("fsm/planning_horizon", planning_horizen_, -1.0); // = 7.5m 预测距离
-    nh.param("fsm/planning_horizen_time", planning_horizen_time_, -1.0); // = 3s 预测时间
-    nh.param("fsm/result_file", result_fn_, string("/home/zuzu/Documents/Benchmark/21-RSS-ego-swarm/2.24/ego/ego_swarm.txt")); // 保存结果文件的地方
-    nh.param("fsm/replan_trajectory_time", replan_trajectory_time_, 0.0); // = 0.1s 重新规划算法的时间
+    // /*  fsm param  */
+    // nh.param("fsm/thresh_replan_time", replan_thresh_, -1.0);  // = 1s
+    // nh.param("fsm/thresh_no_replan_meter", no_replan_thresh_, -1.0); // = 1m
+    // nh.param("fsm/planning_horizon", planning_horizen_, -1.0); // = 7.5m 预测距离
+    // nh.param("fsm/planning_horizen_time", planning_horizen_time_, -1.0); // = 3s 预测时间
+    // nh.param("fsm/result_file", result_fn_, string("/home/zuzu/Documents/Benchmark/21-RSS-ego-swarm/2.24/ego/ego_swarm.txt")); // 保存结果文件的地方
+    // nh.param("fsm/replan_trajectory_time", replan_trajectory_time_, 0.0); // = 0.1s 重新规划算法的时间
 
-    /* initialize main modules */
-    visualization_.reset(new PlanningVisualization(nh));
-    planner_manager_.reset(new EGOPlannerManager);
-    planner_manager_->initPlanModules(nh, visualization_);
-    planner_manager_->deliverTrajToOptimizer(); // store trajectories
-    planner_manager_->setDroneIdtoOpt();
+    // /* initialize main modules */
+    // visualization_.reset(new PlanningVisualization(nh));
+    // planner_manager_.reset(new EGOPlannerManager);
+    // planner_manager_->initPlanModules(nh, visualization_);
+    // planner_manager_->deliverTrajToOptimizer(); // store trajectories
+    // planner_manager_->setDroneIdtoOpt();
 
-    /* callback */
-    // exec_timer_ = nh.createTimer(ros::Duration(0.01), &EGOReplanFSM::execFSMCallback, this); //主程序 先优化吊挂物轨迹 然后优化上面的绳索
+    // /* callback */
+    // // exec_timer_ = nh.createTimer(ros::Duration(0.01), &EGOReplanFSM::execFSMCallback, this); //主程序 先优化吊挂物轨迹 然后优化上面的绳索
 
-    odom_sub_ = nh.subscribe("odom_world", 1, &EGOReplanFSM::odometryCallback, this); //接收到的odom消息，our_lift中我们设定为吊挂物当前位置
+    // odom_sub_ = nh.subscribe("odom_world", 1, &EGOReplanFSM::odometryCallback, this); //接收到的odom消息，our_lift中我们设定为吊挂物当前位置
 
-    //要发给odom的 
-    poly_traj_pub_ = nh.advertise<traj_utils::PolyTraj>("planning/trajectory", 10);
-    start_pub_ = nh.advertise<std_msgs::Bool>("planning/start", 1);
-    reached_pub_ = nh.advertise<std_msgs::Bool>("planning/finish", 1);
+    // //要发给odom的 
+    // poly_traj_pub_ = nh.advertise<traj_utils::PolyTraj>("planning/trajectory", 10);
+    // start_pub_ = nh.advertise<std_msgs::Bool>("planning/start", 1);
+    // reached_pub_ = nh.advertise<std_msgs::Bool>("planning/finish", 1);
 
-    //记录优化结果
-    result_file_.open(result_fn_, ios::app);
+    // //记录优化结果
+    // result_file_.open(result_fn_, ios::app);
 
-    central_goal = nh.subscribe("/move_base_simple/goal", 1, &EGOReplanFSM::formationWaypointCallback, this); //地图上鼠标发来的目标位置
+    // central_goal = nh.subscribe("/move_base_simple/goal", 1, &EGOReplanFSM::formationWaypointCallback, this); //地图上鼠标发来的目标位置
 
   }
 
