6ac9e7c5ee632d1bfe745c5c166d5ac5bbc3da8b
DuanDengyan11 <2560104196@qq.com>
2022-11-08 19:03:39 +0800

    20221108

--- src/planner/traj_opt/src/poly_traj_optimizer.cpp
+++ src/planner/traj_opt/src/poly_traj_optimizer.cpp
@@ -361,7 +361,7 @@ namespace ego_planner
     opt->VirtualTGradCost(T, t, gradT, gradt, time_cost);
 
     fstream process_file_;
-    process_file_.open(string("/home/oem/ddy/process.txt"), ios::app);
+    process_file_.open(string("/home/ddy/process.txt"), ios::app);
     process_file_ << fixed << setprecision(2) <<opt->iter_num_ << "\t" <<obs_swarm_feas_qvar_costs(0) << "\t" <<obs_swarm_feas_qvar_costs(1) << "\t" << obs_swarm_feas_qvar_costs(4) << "\t" <<obs_swarm_feas_qvar_costs(5) <<  "\t" <<  obs_swarm_feas_qvar_costs(2) << "\t" <<obs_swarm_feas_qvar_costs(3) << "\t" << smoo_cost << "\t" << time_cost << "\t" << smoo_cost + obs_swarm_feas_qvar_costs.sum() + time_cost << "\n";
 
     opt->iter_num_ += 1;
