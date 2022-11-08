a4a7514e8f5d03875e9b78f1277d061581c39232
DuanDengyan11 <2560104196@qq.com>
2022-11-08 20:14:17 +0800

    20221108

--- src/planner/cable_load/src/cable_load.cpp
+++ src/planner/cable_load/src/cable_load.cpp
@@ -2,7 +2,7 @@
 
 void cable_load::init(ros::NodeHandle &nh)
 {  
-    // node_ = nh;
+    node_ = nh;
     // node_.param("optimization/load_length", length_ , 1.0);  //吊挂物长宽高
     // node_.param("optimization/load_width", width_ , 1.0);
     // node_.param("optimization/load_height", height_ , 1.0);
